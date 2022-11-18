9a4ddf2249c832886cfa6bc6d46b6804b02e96f8
daniel <danisiliente@gmail.com>
2022-11-18 10:01:40 -0500

    Primer diccionario

---
+++ tuplasDiccionarios/ejemploDiccionario.py
@@ -0,0 +1,8 @@
+pc = {'ram':16,'procesador':8,'vc':6,'ssd':1000,'fuente':650}
+print(pc)
+print(pc['procesador'])
+print(pc['ram'],pc['ssd'])
+del pc['ram']
+print(pc)
+for objeto, valor in pc.items():
+    print('en la pc tenemos',objeto,valor)
