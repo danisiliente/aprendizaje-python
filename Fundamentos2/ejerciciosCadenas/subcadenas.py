2a6825e4ccb3cea77b31fb36dd41ecefe7f04bad
daniel <danisiliente@gmail.com>
2023-02-19 20:21:00 -0500

    Excepciones

--- Fundamentos2/ejerciciosCadenas/subcadenas.py
+++ Fundamentos2/ejerciciosCadenas/subcadenas.py
@@ -1,7 +1,7 @@
 # Imprima todas las subcadenas que salen de una cadena comenzando con las dos primeras letras, 
 # luego tres primeras y así sucesivamente hasta llegar a la última.
 
-def imprimir_subcadenas(cad):
+def subcadenas(cad):
     control = len(cad)
     suma = 0
     for c in range(control - 1):
@@ -9,4 +9,4 @@ def imprimir_subcadenas(cad):
         suma += 1
         print(sub)
 
-imprimir_subcadenas('paralelismos')
+subcadenas('paralelismos')
