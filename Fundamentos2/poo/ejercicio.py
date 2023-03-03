ca4353879827f42fe4594d9ef25d9105e35080e2
daniel <danisiliente@gmail.com>
2023-03-03 09:09:46 -0500

    Ejercicio terminado

--- Fundamentos2/poo/ejercicio.py
+++ Fundamentos2/poo/ejercicio.py
@@ -11,10 +11,10 @@ class Empleado:
         self.__nombre = nombre
         self.__cargo = cargo
         self.__salario = salario
-        Empleado.contador += 1
+        Empleado.contador += 1 # Va aumentando cada vez que se crea o instancia un objeto.
     
     def getAtributos(self):
-        return self.__nombre, self.__cargo, self.__salario
+        return self.__nombre, self.__cargo, self.__salario, Empleado.contador # Retorna al final el número del contador en cada objeto.
 
     def setNombre(self, nombre):
         self.__nombre = nombre
@@ -26,40 +26,45 @@ class Empleado:
         self.__salario = salario
 
     def calcSalario(self):
-        salario = self.__salario
-        self.__salario = salario
-        salPorDia = salario // 30
-        salPorHora = salPorDia // 8
-        return salPorHora
+        self.salario = self.__salario # almacena el valor del salario que se le pasa como argumento en el constructor.
+        self.salPorDia = self.salario // 30
+        self.salPorHora = self.salPorDia // 8 # Suponiendo que trabaja 8 horas diarias.
+        return self.salPorHora
 
     def incremento(self):
         salario = self.__salario
-        if salario > 1160000:
-            salario *= 0.13
-            print(f'El IPC aumenta en 13% --> {salario}')
-        else:
+        if salario == 1160000: 
             salario *= 0.16
-            print(f'El IPC aumenta en 16% --> {salario}')
+            print(f'El IPC aumenta en 16% --> {salario}') # Si es el mínimo, aumenta 16% para el siguiente año.
+        elif salario > 1160000:
+            salario *= 0.13
+            print(f'El IPC aumenta en 13% --> {salario}') # Si es más del mínimo, aumenta en 13% para el siguiente año.
+        else:
+            print('El salario no puede ser menor al mínimo.') # No se puede calcular el IPC si el pago es menos del mínimo.
 
     def horasExtra(self, horas):
-        salario = self.__salario
-        salPorDia = salario // 30
-        salPorHora = salPorDia // 24
         if horas <= 40:
-            suma = horas * salPorHora
-            salario += horas * salPorHora
+            suma = horas * self.salPorHora # Se utilizan las variables de calcSalario.
+            self.salario += horas * self.salPorHora # Se utilizan las variables de calcSalario.
             print(f'Pago por horas extra = {suma}')
-            print(f'Pago de horas extra + salario = {salario}')
-        elif horas > 40:
-            print('El total de horas no puede ser mayor a 40 horas mensuales.')
+            print(f'Pago de horas extra + salario = {self.salario} \n')
         else:
-            print('Dato incorrecto.')
+            print('El total de horas no puede ser mayor a 40 horas mensuales. \n')
 
-ob1 = Empleado('Akiles', 'Scrum Master', 1160000)
+ob1 = Empleado('Akiles', 'Diseñador', 1160000)
 print(ob1.getAtributos())
 print(ob1.calcSalario())
 ob1.incremento()
 ob1.horasExtra(1)
 
-ob1.setNombre('Héctor')
-print(ob1.getAtributos())
+ob2 = Empleado('Héctor', 'Publicista', 2320000)
+print(ob2.getAtributos())
+print(ob2.calcSalario())
+ob2.incremento()
+ob2.horasExtra(40)
+
+ob3 = Empleado('Ajáx', 'Pasante', 580000)
+print(ob3.getAtributos())
+print(ob3.calcSalario())
+ob3.incremento()
+ob3.horasExtra(80)
