ed3a043ad22ddf006daa8610a703688fc1b668c1
T4RS <danisiliente@gmail.com>
2023-10-12 10:05:17 -0500

    Crud completo con Django

--- Web/Django_Python/libreria/views.py
+++ Web/Django_Python/libreria/views.py
@@ -7,16 +7,30 @@ from .forms import UsuarioForm
 
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
 def libros(request):
     usuarios = Usuario.objects.all()
     return render(request, 'libros/index.html', {'usuarios': usuarios})
+
 def crear(request):
     formulario = UsuarioForm(request.POST or None, request.FILES or None)
     if formulario.is_valid():
         formulario.save()
         return redirect('libros')
     return render(request, 'libros/crear.html', {'formulario': formulario})
-def editar(request):
-    return render(request, 'libros/editar.html')
+
+def editar(request, id):
+    usuario = Usuario.objects.get(id = id)
+    formulario = UsuarioForm(request.POST or None, request.FILES or None, instance = usuario)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/editar.html', {'formulario': formulario})
+
+def eliminar(request, id):
+    usuario = Usuario.objects.get(id = id)
+    usuario.delete()
+    return redirect('libros')
