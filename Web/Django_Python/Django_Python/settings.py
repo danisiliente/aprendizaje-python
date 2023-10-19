f83f7efa65bb6c6383135ca934370a96b1c4111e
Aprendiz <Aprendiz@SOAAPRCDIFSD046.FormacionSena.RED>
2023-10-19 15:16:00 -0500

    Actualización Django

--- Web/Django_Python/Django_Python/settings.py
+++ Web/Django_Python/Django_Python/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -126,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/img/",
+]
